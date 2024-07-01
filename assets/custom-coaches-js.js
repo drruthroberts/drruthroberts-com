4787df23df5a6e788df99a7c0f422059138316c1
drruthroberts <drruthroberts@drruthroberts.com>
2024-07-01 15:58:34 -0500

    inital commit

---
+++ assets/custom-coaches-js.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+   if(window.location.href.indexOf("page") > -1) {
+     if($('body').is('#holistic-pet-health-coach-consultation')) {
+       $('html,body').animate({
+        scrollTop: $("#shopify-section-template--14940389605441__main-collection-product-grid").offset().top},
+        'slow');
+     }
+    }
+});
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+  $j("#coaches .grid__item:nth-child(1)").click(function() {
+    window.open('https://drruthroberts.com/products/test-product-4?_pos=1&_fid=e0bd9f4ca&_ss=c');
+  });
+});
+
+var $j2 = jQuery.noConflict();
+
+$j2(document).ready(function() {
+  $j2("#coaches .grid__item:nth-child(2)").click(function() {
+    window.open('https://drruthroberts.com/products/test-product-3?_pos=2&_fid=9b8cab1de&_ss=c');
+  });
+});
+
+var $j3 = jQuery.noConflict();
+
+$j3(document).ready(function() {
+  $j3("#coaches .grid__item:nth-child(3)").click(function() {
+    window.open('https://drruthroberts.com/products/coaches-2?_pos=3&_fid=5f802496d&_ss=c');
+  });
+});
+
+var $j4 = jQuery.noConflict();
+
+$j4(document).ready(function() {
+  $j4("#coaches .grid__item:nth-child(4)").click(function() {
+    window.open('https://drruthroberts.com/products/coaches-1?_pos=4&_fid=5f802496d&_ss=c');
+  });
+});
+
+ 
+    
+$("#coaches .card__heading a").removeAttr('href');
+$("#coaches .card__content a").removeAttr('href');
+
+
+document.getElementById('Details-3-template--14940389605441__main-collection-product-grid').addEventListener("click", function() {
+
+
+
+});
+
+window.addEventListener("load", (event) => {
+
+  document.getElementsByClassName("facets__disclosure-vertical").click();
+
+  $(document).ready(function(){
+    $(".facets__disclosure-vertical").trigger('click');
+    });
+  
+
+
+
+  
+});
+
+ 
