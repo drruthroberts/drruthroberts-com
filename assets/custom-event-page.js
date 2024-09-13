d42067bfacb11fda1eb18af7a9a987e47892f899
drruthroberts <drruthroberts@drruthroberts.com>
2024-09-12 20:02:16 -0500

    created event page and updated sort by option

---
+++ assets/custom-event-page.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function() {
+    var second = 1000, minute = second * 60, hour = minute * 60, date = hour * 24;
+    let year = document.querySelector('.year').dataset.value;
+    let month = document.querySelector('.month').dataset.value;
+    let startDate = document.querySelector('.date').dataset.value;
+    let startHour = document.querySelector('.start-hour').textContent;
+    
+    const countDown = new Date(year, month, startDate, startHour);
+    x = this.setInterval(function(){
+        var now = new Date().getTime();
+        var distance = countDown - now;
+        Math.floor(distance/(date));
+        document.querySelectorAll('.js-timer-days').forEach(dateItem=>{
+            dateItem.innerText = Math.floor(distance/(date));
+        })
+        document.querySelectorAll('.js-timer-hours').forEach(hourItem=>{
+            hourItem.innerText = Math.floor((distance % (date)) / (hour));
+        })
+        document.querySelectorAll('.js-timer-minutes').forEach(minuteItem=>{
+            minuteItem.innerText = Math.floor((distance % (hour)) / (minute));
+        })
+        document.querySelectorAll('.js-timer-seconds').forEach(secondItem=>{
+            secondItem.innerText = Math.floor((distance % (minute)) / (second));
+        })
+        
+        
+    }, second)
+    
+})
---
+++ assets/custom-slider-banner.js
@@ -0,0 +1,12 @@
+new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 500,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+  });
